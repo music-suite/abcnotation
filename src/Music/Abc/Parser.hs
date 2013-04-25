a41cfc027eb7e851fe31b07de8bdc173bd546d76
Hans Höglund <hans@hanshoglund.se>
2013-04-25 20:47:07 +0200

    Basic module layout

--- src/Music/Abc/Parser.hs
+++ src/Music/Abc/Parser.hs
@@ -12,7 +12,59 @@
 -------------------------------------------------------------------------------------
 
 module Music.Abc.Parser (
+    parse
   ) where
 
+import Control.Monad
+import Control.Applicative hiding ((<|>), optional, many)
+
+import Text.Parsec hiding (parse)
+import Text.Parsec.Token
+import Text.Parsec.String
+
+import Music.Abc
+
+parse :: String -> Abc
+parse = notSupported "parse"
 
 
+-------------------------------------------------------------------------------------
+-- Lexer
+-------------------------------------------------------------------------------------
+
+lexer :: TokenParser ()
+lexer = makeTokenParser $ LanguageDef {
+    commentStart    =  "/*",
+    commentEnd      =  "*/",
+    commentLine     =  "//",
+    nestedComments  =  True,
+    identStart      =  (letter <|> char '_'),
+    identLetter     =  (alphaNum <|> char '_'),
+    opStart         =  mzero,
+    opLetter        =  mzero,
+    reservedNames   =  reservedNames,
+    reservedOpNames =  mzero,
+    caseSensitive   =  True
+    }
+    where
+        reservedNames = [
+            "module", "import", "type", "tagname", "opaque", "enum", "union", "struct", "bitfield",
+            "Int", "Void", "Size", "Ptrdiff", "Intptr", "UIntptr",
+            "Char", "Short", "Int", "Long", "LongLong",
+            "UChar", "UShort", "UInt", "ULong", "ULongLong",
+            "Float", "Double", "LongDouble",
+            "Int8", "Int16", "Int32", "Int64", "UInt8", "UInt16", "UInt32", "UInt64" ]
+
+-- Convenient synonyms, not exported
+llex   = lexeme lexer
+lnat   = natural lexer
+lstr   = stringLiteral lexer
+lname  = identifier lexer
+lres   = reserved lexer
+lspace = whiteSpace lexer
+
+
+single x = [x]
+
+notSupported x = error $ "Not supported yet: " ++ x
+                                                         
