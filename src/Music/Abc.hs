211baf7c74093a9ddc910cc4ee7f0731bc931e36
Hans Höglund <hans@hanshoglund.se>
2013-08-26 01:44:45 +0200

    Adds test

--- src/Music/Abc.hs
+++ src/Music/Abc.hs
@@ -21,7 +21,8 @@ module Music.Abc (
         -- * Abc format
         -- ** Files
         AbcFile(..),
-
+        noVersion,
+        noHeader,
         -- *** File header
         FileHeader(..),
         Element(..),
@@ -90,6 +91,11 @@ data AbcFile
         (Maybe String)
         (Maybe FileHeader)
         [Element]
+    deriving (Eq, Ord, Show)
+
+
+noVersion = Nothing
+noHeader  = Nothing
 
 -- | File header (2.2.2).
 data FileHeader
@@ -308,7 +314,7 @@ data Information
 
 type Key = (PitchClass, Mode)
 
--- | Optional string, beats, frequency (3.1.8)
+-- | Optional string, numerators, frequency (3.1.8)
 type Tempo = (Maybe String, [Duration], Duration)
 
 type Symbol = ()
@@ -351,6 +357,63 @@ readAbc = error "Not impl"
 
 showAbc :: AbcFile -> String
 showAbc = error "Not impl"
+
+
+{-
+    
+    X:19004
+    T:Silent Night
+    T:Stille Nacht! Heilige Nacht!
+    R:Air
+    C:Franz Xaver Gruber, 1818
+    O:Austria
+    Z:Paul Hardy's Xmas Tunebook 2012 (see www.paulhardy.net). Creative Commons cc by-nc-sa licenced.
+    M:6/8
+    L:1/8
+    Q:3/8=60
+    K:C
+    "C"G>A G E3|G>A G E2z|"G"d2 d B3|"C"c2 c G2z|
+    "F"A2 A c>B A|"C"G>A G E2z|"F"A2 A c>B A|"C"G>A G E2z|
+    "G7"d2 d f>d B|"C"c3 e2z|cGE "G7"G>F D|"C"C3-C3|]
+    W:Silent night, holy night
+    W:All is calm, all is bright
+    W:Round yon Virgin Mother and Child
+    W:Holy Infant so tender and mild
+    W:Sleep in heavenly peace
+    W:Sleep in heavenly peace
+    W:
+    W:(Josef Mohr, 1818, Trans by John Young, 1819)
+    
+-}
+test = AbcFile 
+    (Just "1.2") 
+    (Just $ FileHeader [ 
+            ReferenceNumber     19004,
+            TuneTitle           "Silent Night",
+            TuneTitle           "Stille Nacht! Heilige Nacht!",
+            Rhythm              "Air",
+            Composer            "Franz Xaver Gruber, 1818",
+            Origin              "Austria",
+            Source              "Paul Hardy's Xmas Tunebook 2012",
+            Meter               (Simple $ 6/8),
+            UnitNoteLength      (1/8),
+            Tempo               (Nothing, [3/8], 60),
+            Key                 (C, Major),            
+
+            Words               "Silent night, holy night",
+            Words               "All is calm, all is bright",
+            Words               "Round yon Virgin Mother and Child",
+            Words               "Holy Infant so tender and mild",
+            Words               "Sleep in heavenly peace",
+            Words               "Sleep in heavenly peace"
+        ] []) 
+    [
+    
+    ]
+
+
+
+
 
 infixr 5 :|:
 type a :|: b = Either a b
